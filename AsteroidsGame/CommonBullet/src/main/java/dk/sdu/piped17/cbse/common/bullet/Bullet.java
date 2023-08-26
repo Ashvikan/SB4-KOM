fa7c77ac8ef5c43e5053160f2df6b9424e6f31f5
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2023-08-26 16:59:54 +0200

    updating files and correcting few mistakes

---
+++ AsteroidsGame/CommonBullet/src/main/java/dk/sdu/piped17/cbse/common/bullet/Bullet.java
@@ -0,0 +1,6 @@
+package dk.sdu.assiv20.cbse.common.bullet;
+
+import dk.sdu.assiv20.cbse.common.data.Entity;
+
+public class Bullet extends Entity {
+}
---
+++ AsteroidsGame/Player/src/main/java/dk/sdu/assiv20/cbse/playersystem/Player.java
@@ -0,0 +1,11 @@
+package dk.sdu.assiv20.cbse.playersystem;
+
+import dk.sdu.assiv20.cbse.common.data.Entity;
+
+/**
+ *
+ * @author corfixen
+ */
+public class Player extends Entity {
+
+}
